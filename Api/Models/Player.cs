c4e6d04517de6c4228fb80e40e06120b71a93d39
Unknown <asser.salmela@gmail.com>
2018-09-26 13:46:48 +0300

    Player class

--- Api/Models/Player.cs
+++ Api/Models/Player.cs
@@ -1,7 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 namespace Api.Models
+
 {
     public class Player
     {
-        
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public bool IsBanned { get; set; }
+        public DateTime CreationTime { get; set; }
+        public List<Player> friendList = new List<Player>();
     }
+
+    public class NewPlayer
+    {
+        public string Name { get; set; }
+    }
+
+        public class ModifiedPlayer
+    {
+        public int Score { get; set; }
+    }
+
+    public class BannedPlayer
+    {
+        public bool IsBanned { get; set; }
+    }
+
 }
